1705d9ded073115ca534cbb78291fc28df81c9c1
arinsarker07-oss <arinsarker07@gmail.com>
2026-06-29 21:21:32 +0600

    final commit

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     const database = client.db("NEXT-HOME");
