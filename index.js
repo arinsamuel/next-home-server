28dad75c8ac11dea68b5086b7242619af1221e94
arinsarker07-oss <arinsarker07@gmail.com>
2026-06-30 06:54:03 +0600

    fix cors

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 dotenv.config();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://next-home-nu.vercel.app", 
+  credentials: true 
+}));
 app.use(express.json());
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
